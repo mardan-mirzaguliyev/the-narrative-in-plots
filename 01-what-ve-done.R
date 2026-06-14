dcffc6cb5b6801f56b848753403a9058684b0ddf
Mardan Mirzaguliyev <mardan.mirzaguliyev@gmail.com>
2026-06-15 00:48:54 +0400

    Refined code. Added custom theme elements to the graph. Removed redundant variables.

--- 01-what-ve-done.R
+++ 01-what-ve-done.R
@@ -12,40 +12,46 @@ lyrics_id <- "13dM_5vWQGdAeRI-thE6nbzlZIbvbZ8dw9eCu3MsP9Kk"
 lyrics <- read_sheet(lyrics_id)
 
 # 1. Tokenize your sanitized dataset
-tokenized_lyrics <- lyrics %>%
-  unnest_tokens(output = word, input = text)
 
 ## 1.1 Count total words 
-total_words <- nrow(tokenized_lyrics)
-total_words
+lyrics %>%
+  unnest_tokens(output = word, input = text) |> 
+  nrow()
 
-## 1.2 Sentiment analysis of line by line
-### 1.2.1 First two lines
 
-lyrics |> slice_head(n = 2) |> 
-    unnest_tokens(output = word, input = text) |>
-    inner_join(get_sentiments("bing"), by = "word")
-
-### 1.2.3 Line 3 and Line 4
-
-lyrics |> slice(2:4) |>
+lyrics |>
   unnest_tokens(output = word, input = text) |>
   inner_join(get_sentiments("bing"), by = "word") |> 
   count(line, sentiment) |> 
-  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) |>
-  mutate(line = as.integer(line)) |> 
-  (\(df) mutate(df, positive = ifelse("positive" %in% names(df), positive <- positive, 0)))() |> 
-  (\(df) mutate(df, negative = ifelse("negative" %in% names(df), negative <- negative, 0)))() |> 
-  mutate(sentiment_score = positive - negative) |> 
+  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0) |> 
+  (\(df) {
+    if (!"positive" %in% names(df)) df$positive <- 0
+    if (!"negative" %in% names(df)) df$negative <- 0
+    mutate(df, sentiment_score = positive - negative)
+  })() |> 
   ggplot(aes(x = line, y = sentiment_score)) +
   geom_col(aes(fill = sentiment_score > 0), show.legend = FALSE) +
-  scale_fill_manual(values = c("TRUE" = "#2a9d8f", "FALSE" = "#bd1515")) + 
+  scale_fill_manual(values = c("TRUE" = "#2a9d8f", "FALSE" = "#bd1515")) +
+  scale_x_continuous(breaks = seq(min(line_sentiment$line), max(line_sentiment$line), by = 1)) +
   theme_minimal(base_size = 14) +
   labs(
     title = "'What I've Done' ",
     subtitle = "Line-by-line emotional trajectory showing narrative shifts",
     x = "Song Timeline (Line Number)",
     y = "Net Sentiment Score"
+  ) +
+  theme(
+    plot.title = element_text(size = 16, face = "bold", hjust = 0.5, color = "black"),
+    plot.subtitle = element_text(size = 11, hjust = 0.5, color = "black"),
+    axis.text.y = element_text(size = 12, face = "bold", color = "black"),
+    axis.text.x = element_text(size = 9, color = "black"),
+    axis.title.x = element_text(color = "black", size = 11),
+    legend.position = "none",
+    panel.grid.major.y = element_blank(),
+    panel.grid.major.x = element_line(color = "gray30", linewidth = 0.1),
+    panel.grid.minor = element_blank(),
+    plot.background = element_rect(fill = "#cbe8f5", color = NA),
+    panel.background = element_rect(fill = "#cbe8f5", color = NA)
   )
 
 
@@ -53,41 +59,6 @@ lyrics |> slice(2:4) |>
 
 
 
-
-
-## 1.3 Sentiment analysis of full text
-word_sentiments <- tokenized_lyrics |> 
-  inner_join(get_sentiments("bing"), by = "word")
-word_sentiments
-
-### 1.3.1 Calculate a net sentiment score per line of the song
-line_sentiment_raw <- word_sentiments %>%
-  count(line, sentiment) %>%
-  pivot_wider(names_from = sentiment, values_from = n, values_fill = 0)
-
-if (!"positive" %in% names(line_sentiment_raw)) line_sentiment_raw$positive <- 0
-if (!"negative" %in% names(line_sentiment_raw)) line_sentiment_raw$negative <- 0
-
-
-# 4. Final calculation of the net score
-
-line_sentiment_raw
-line_sentiment <- line_sentiment_raw %>%
-  mutate(sentiment_score = positive - negative)
-
-line_sentiment
-
-
-ggplot(line_sentiment, aes(x = line, y = sentiment_score)) +
-  geom_col(aes(fill = sentiment_score > 0), show.legend = FALSE) +
-  scale_fill_manual(values = c("TRUE" = "#2a9d8f", "FALSE" = "#bd1515")) + 
-  theme_minimal(base_size = 14) +
-  labs(
-    title = "The Rolling Sentiment of 'What I've Done'",
-    subtitle = "Line-by-line emotional trajectory showing narrative shifts",
-    x = "Song Timeline (Line Number)",
-    y = "Net Sentiment Score"
-  )
 
 
 
